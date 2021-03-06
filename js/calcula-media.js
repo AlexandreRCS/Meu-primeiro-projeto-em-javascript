a6559b6204435d022376fb2033ad75bfef3c958f
AlexandreRCS <77364739+AlexandreRCS@users.noreply.github.com>
2021-03-06 18:00:31 -0300

    Att-06-03-2021
    Adicionado a média e situação do aluno

--- js/calcula-media.js
+++ js/calcula-media.js
@@ -1,9 +1,27 @@
-var aluno = document.querySelector('.alunos');
-var notas = aluno.querySelectorAll('.info-nota');
-var resulado =  aluno.querySelector('.info-media');
-console.log(notas)
 
-var media = calculaMedia(aluno);
+var alunos = document.querySelectorAll('.alunos');
+
+adicionaMedia(alunos);
+
+function adicionaMedia(alunos){
+    alunos.forEach(function(aluno){
+        var media = calculaMedia(aluno);
+        var tdMedia = aluno.querySelector('.info-media');
+        tdMedia.textContent = media.toFixed(1);
+        var situacao = aluno.querySelector('.info-situacao');
+        if(media > 7.0){
+            situacao.classList.add('aluno-aprovado')
+            situacao.textContent = 'Aprovado';
+        }else if(media > 5.0 && media <= 7.0){
+            situacao.classList.add('aluno-recuperacao')
+            situacao.textContent = 'Recuperação';
+        }else{
+            situacao.classList.add('aluno-reprovado')
+            situacao.textContent = 'Reprovado';
+        }
+        
+    });
+}
 
 
 function calculaMedia(aluno){
@@ -18,11 +36,8 @@ function calculaMedia(aluno){
 }
 
 
-console.log(media)
 
-resulado.textContent = media;
-
-
-
-
+function adicionaSituacao(nota){
+  
+}
 
