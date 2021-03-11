fae2d620858bad0b2d262ee5c353422e185f14c8
AlexandreRCS <77364739+AlexandreRCS@users.noreply.github.com>
2021-03-11 18:39:28 -0300

    Att-
    continuando a função de remover aluno

--- js/remove.js
+++ js/remove.js
@@ -1,14 +1,22 @@
 var tabela = document.querySelector("#tabela-alunos");
 
 tabela.addEventListener('click',function(mode){
-    console.log(tabela)
     selecionaAluno(mode);  
 })
 
+
+tabela.addEventListener('dblclick', function(evento){
+    removeSelecinadoDoAluno(evento);
+})
 
 function selecionaAluno(evento){
     console.log(evento)
     var aluno = evento.target.parentNode;
     aluno.classList.add('seleciona-aluno');
     return aluno;
+}
+
+function removeSelecinadoDoAluno(evento){
+    var aluno = evento.target.parentNode;
+    aluno.classList.remove('seleciona-aluno');
 }
